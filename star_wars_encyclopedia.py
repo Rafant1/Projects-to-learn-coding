76324c615dc1ba8e874df0b26fd73ac808e9f5a9
Rafał Antczak <rantczak112@wp.pl>
2020-11-23 14:04:15 +0100

    changed the name of the get_people function to get_person and normalised the strings form between two main functions

--- star_wars_encyclopedia.py
+++ star_wars_encyclopedia.py
@@ -3,10 +3,10 @@
 import requests
 
 
-def get_people():
+def get_person():
     r1 = requests.get('https://swapi.dev/api/people/')
     r1_dict = r1.json()
-    print('About which person you would like to get more information?')
+    print('About which person would you like to get more information?')
     for person in r1_dict['results']:
         print(person['name'])
 
@@ -23,8 +23,8 @@ def get_people():
             print('eye color:',person['eye_color'])
 
             home_world = requests.get(person['homeworld'])
-            home_world = home_world.json()
-            print('homeworld:',home_world['name'])
+            home_world_dict = home_world.json()
+            print('homeworld:',home_world_dict['name'])
 
             films_list = []
             for element in person['films']:
@@ -38,7 +38,7 @@ def get_people():
 def get_planet():
     r2 = requests.get('https://swapi.dev/api/planets/')
     r2_dict = r2.json()
-    print('About which planet you would like to get more information?')
+    print('About which planet would you like to get more information?')
 
     for planet in r2_dict['results']:
         print(planet['name'])
@@ -46,13 +46,13 @@ def get_planet():
     player_choice = input('Planet\'s name: ')
     for planet in r2_dict['results']:
         if planet['name'] == player_choice:
-            print("\nplanet's name:", planet['name'])
-            print("rotation period [in hours]:", planet['rotation_period'])
-            print("orbital period [in days]:", planet['orbital_period'])
-            print("diameter [in kilometers]:", planet['diameter'])
-            print("climate:", planet['climate'])
-            print("primary terrain[s]:", planet['terrain'])
-            print("population size:", planet['population'])
+            print('\nplanet\'s name:', planet['name'])
+            print('rotation period [in hours]:', planet['rotation_period'])
+            print('orbital period [in days]:', planet['orbital_period'])
+            print('diameter [in kilometers]:', planet['diameter'])
+            print('climate:', planet['climate'])
+            print('primary terrain[s]:', planet['terrain'])
+            print('population size:', planet['population'])
 
             name_list = []
 
@@ -68,5 +68,5 @@ def get_planet():
                 print("There are not any known fameous people born on this planet.")
 
 
-get_planet()
+get_person()
 
