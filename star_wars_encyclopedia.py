a10b3c41c6d786d7c5a3ffea849b59a1f90ffcec
Rafał Antczak <rantczak112@wp.pl>
2020-11-23 13:51:33 +0100

    created get_people function which allows the user get info about specific person from Star Wars saga, using external API

--- star_wars_encyclopedia.py
+++ star_wars_encyclopedia.py
@@ -3,6 +3,38 @@
 import requests
 
 
+def get_people():
+    r1 = requests.get('https://swapi.dev/api/people/')
+    r1_dict = r1.json()
+    print('About which person you would like to get more information?')
+    for person in r1_dict['results']:
+        print(person['name'])
+
+    player_choice = input('Person\'s name: ')
+    for person in r1_dict['results']:
+        if person['name'] == player_choice:
+            print('name:',person['name'])
+            print('gender:',person['gender'])
+            print('birth year:', person['birth_year'])
+            print('height [in centimeters]:',person['height'])
+            print('mass: [in kilograms]',person['mass'])
+            print('hair color:',person['hair_color'])
+            print('skin color:',person['skin_color'])
+            print('eye color:',person['eye_color'])
+
+            home_world = requests.get(person['homeworld'])
+            home_world = home_world.json()
+            print('homeworld:',home_world['name'])
+
+            films_list = []
+            for element in person['films']:
+                element = requests.get(element)
+                films_dict = element.json()
+                films_list.append(films_dict['title'])
+            print('Appearance(s) in films: ', end= '')
+            print(*films_list, sep= ', ')
+
+
 def get_planet():
     r2 = requests.get('https://swapi.dev/api/planets/')
     r2_dict = r2.json()
@@ -10,8 +42,8 @@ def get_planet():
 
     for planet in r2_dict['results']:
         print(planet['name'])
-    player_choice = input('Planet\'s name: ')
 
+    player_choice = input('Planet\'s name: ')
     for planet in r2_dict['results']:
         if planet['name'] == player_choice:
             print("\nplanet's name:", planet['name'])
@@ -34,6 +66,7 @@ def get_planet():
                 print(*name_list, sep=', ')
             else:
                 print("There are not any known fameous people born on this planet.")
+
 
 get_planet()
 
